b9062a24c076dd2262c8b0001a7f205c28a26c2a
Adi <72514473+anr2311@users.noreply.github.com>
2025-09-05 15:17:01 +0000

    trying out stuff

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,17 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main()
+{
+    int a = 5; // declaration statement
+    int b = 5; // declaration statement
+
+    int c = a + b; // declaration statement with an expression
+
+    if (c > 5) {} // conditional statement with expression
+
+    return 0; // return statement
+}
