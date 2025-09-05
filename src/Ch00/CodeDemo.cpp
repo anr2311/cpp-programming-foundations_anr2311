5a524a5a94e75900926a0c3eab5e2ad35edb0232
Adi <72514473+anr2311@users.noreply.github.com>
2025-09-05 15:01:14 +0000

    additional commits

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -3,9 +3,19 @@
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 // comment by anr
+#define ANR_MOD 1
 
-#include <iostream>
+#include <iostream> /* pre-processor directive --> using a library to receive / print text to the screen */
 
+#if (ANR_MOD)
+int main(){
+    /* std : library, :: scope resolution & cout object */
+    /* << left bound insertion operator */
+    std::cout << "Hello World!\n" << std::endl;
+
+    return 0; /* returning 0 is to tell no errors in exec */
+}
+#else
 int main(){
     float num_1, num_2, result;
 
@@ -21,3 +31,4 @@ int main(){
     std::cout << std::endl << std::endl;
     return 0;
 }
+#endif
