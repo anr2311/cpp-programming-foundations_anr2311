6dae583aecefd248094df2d22fb7270306338c4c
Adi <72514473+anr2311@users.noreply.github.com>
2025-09-06 17:36:45 +0000

    updated

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,7 +4,24 @@
 
 #include <iostream>
 
+int a, b = 5; /* they are global variable --> accessible by
+all parts of the code (memory managed statically by compiler) unallocated once the program ends */
+
 int main(){
+
+    bool my_flag;
+
+    a = 7;
+    my_flag = false;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
+
+    my_flag = true;
+
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
