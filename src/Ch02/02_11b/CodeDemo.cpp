95ee4eff59a6d75463a664bd63a9557476f8f5a1
Adi <72514473+anr2311@users.noreply.github.com>
2025-09-06 18:03:44 +0000

    further edits

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -7,20 +7,20 @@
 
 int main(){
     int fahrenheit = 100;
-    int celsius;
+    float celsius;
 
-    celsius = fahrenheit;
+    celsius = (5.0/9.0) * (fahrenheit - 32);
 
     std::cout << std::endl;
     std::cout << "Fahrenheit: " << fahrenheit << std::endl;
-    std::cout << "Celsius   : " << celsius << std::endl;
+    std::cout << "Celsius   : " << static_cast<float>(celsius) << std::endl;
 
     float weight = 10.99;
     
     std::cout << std::endl;
     std::cout << "Float          : " << weight << std::endl;
-    std::cout << "Integer part   : " << weight << std::endl;
-    std::cout << "Fractional part: " << weight << std::endl;
+    std::cout << "Integer part   : " << static_cast<int32_t>(weight) << std::endl;
+    std::cout << "Fractional part: " << (weight - static_cast<int32_t>(weight)) << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
