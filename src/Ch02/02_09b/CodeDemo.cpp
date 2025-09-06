21c12b356bb3fe0bf3b7020e63494106bc1db636
Adi <72514473+anr2311@users.noreply.github.com>
2025-09-06 17:48:27 +0000

    enum classes

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -7,8 +7,25 @@
 
 enum class character_role {protagonist, antagonist, sidekick, npc};
 
+struct game_characters
+{
+    std::string name;
+    int level;
+    character_role role;
+};
+
 int main(){
     
+    game_characters buddy;
+
+    buddy.name = "ANR";
+    buddy.level = 100;
+    buddy.role = character_role::sidekick;
+
+    std::cout << "Name: " << buddy.name << std::endl;
+    std::cout << "Level: " << buddy.level << std::endl;
+    std::cout << "Role: " << (int)buddy.role << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
