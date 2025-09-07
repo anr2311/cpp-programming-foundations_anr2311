db4acd5f73d5ae1ba0c2e3945088f002bcc2c902
Adi <72514473+anr2311@users.noreply.github.com>
2025-09-07 16:15:35 +0000

    updated

--- src/Ch03/03_05b/CodeDemo.cpp
+++ src/Ch03/03_05b/CodeDemo.cpp
@@ -12,14 +12,24 @@ int main(){
     std::cout << "Initial Zombies Incoming: " << incoming_zombies << std::endl;
 
     // Addition assignment
+    ammo += 10;
+    std::cout << "New Ammo found: " << ammo << std::endl;
 
     // Subtraction assignment
+    ammo -= 5;
+    std::cout << "Ammo after some use: " << ammo << std::endl;
 
     // Multiplication assignment
+    incoming_zombies *= 2;
+    std::cout << "New Zombies Incoming: " << incoming_zombies << std::endl;
 
     // Division assignment
+    ammo /= 2;
+    std::cout << "Ammo shared with friend: " << ammo << std::endl;
 
     // Remainder assignment
+    ammo %= 7; // when player loses his ammo pack, left with remainder of ammo in his gun
+    std::cout << "Ammo pack lost: " << ammo << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -2,12 +2,21 @@
 // Exercise 03_06
 // Preprocessor Directives, by Eduardo Corpeño 
 
-#include <iostream>
+#include <iostream> /* (no .h or other extensions) look for file in default include folder */
 #include <cstdint>
 
+#define MAX_AMMO 500 /* pre-processor will replace MAX_AMMO in any part of the code with constant value 500 */
+
+#define DEBUG
+
 int main(){
-    int32_t ammo = 100;
+    /* the below types are portable across architectures unlike 'int' */
+    int32_t ammo = MAX_AMMO / 5;
     uint8_t health_items = 5;
+
+#ifdef DEBUG
+    std::cout << "[DEBUG] Game starting here" << std::endl;
+#endif
 
     ammo += 200; // Player finds extra ammo
     health_items -= 2; // Player uses some health items
