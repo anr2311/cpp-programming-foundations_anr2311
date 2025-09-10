6f89b9a6921f2251411924119d681b4edc944c3e
Adi <72514473+anr2311@users.noreply.github.com>
2025-09-10 15:53:10 +0000

    updated

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -4,7 +4,18 @@
 
 #include <iostream>
 
+#define MAX_ARRAY_LENGTH 4u
+
 int main(){
+
+    /* auto keyword doesnt work for arrays */
+    int enemies[MAX_ARRAY_LENGTH] = {10, 15, 20, 15};
+
+    std::cout << "The game as 4 level enemies" << std::endl;
+    std::cout << enemies[0] << std::endl;
+    std::cout << enemies[1] << std::endl;
+    std::cout << enemies[2] << std::endl;
+    std::cout << enemies[3] << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
