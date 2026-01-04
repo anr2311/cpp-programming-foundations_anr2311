718b0583c663f62c1d9817afc28b7fdbeeec46bd
Adi <72514473+anr2311@users.noreply.github.com>
2026-01-04 07:37:10 +0000

    files

--- src/Ch09/09_02b/CodeDemo.cpp
+++ src/Ch09/09_02b/CodeDemo.cpp
@@ -17,8 +17,15 @@ int main(){
         std::cout << std::endl << "File not found!" << std::endl;
     else{
         while (!inFile.eof()){
+            // assuming file always has the same format
             getline(inFile, str);
-            std::cout << str << std::endl;
+            std::cout << str << ", ";
+            getline(inFile, str);
+            number = stoi(str);
+            std::cout << number << ", ";
+            getline(inFile, str);
+            letter = str[0];
+            std::cout << letter << std::endl;
         }
         inFile.close();
     }
